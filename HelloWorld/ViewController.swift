3888f67204e01b3f6f2e9a6263e3602479c0cced
Aisyah Bellia <aisyah.belia@hreasily.com>
2019-11-21 16:05:28 +0700

    HelloWorld

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Aisyah Bellia on 14/10/19.
+//  Copyright © 2019 Aisyah Bellia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
